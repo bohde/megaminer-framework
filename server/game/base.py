541fc0952195eac64a6566425b31dac8022246a0
Josh Bohde <josh.bohde@gmail.com>
2009-07-21 16:48:31 -0500

    Base objects are tested. Can make a game world now.

--- server/game/base.py
+++ server/game/base.py
@@ -2,27 +2,33 @@ import json
 import itertools
 import collections
 
-def lookupFactory(max_x, max_y):
-    """
-    if something isn't there return a new list
-    if out of bounds, return IndexError
-    """
-    def lookup(coord):
-        if not(isinstance(coord, collections.Sequence)):
-            raise TypeError('%s is not a subclass of Sequence.' % n.__class__)
-        if len(coord) != 2:
-            raise TypeError('A coordinate does not have a length of %n!' % len(coord))
-        x, y = coord
-        if abs(x) > max_x or abs(y) > max_y:
-            raise IndexError('(%n, %n) is not within (-+%n, -+%n)' % (x, y, max_x, max_y))
-        return []
-    return lookup
+class RectangularArea(collections.defaultdict):
+
+    def __init__(self, max_x, max_y):
+        collections.defaultdict.__init__(self, list)
+        self.max_x = max_x
+        self.max_y = max_y
 
 
-def standard(x, y):
+    def __missing__(self, key):
+        if not(isinstance(key, collections.Sequence)):
+                raise TypeError('%s is not a subclass of Sequence.' % key.__class__)
+        if len(key) != 2:
+            raise TypeError('A coordinate does not have a length of %u!' % len(key))
+        x, y = key
+        if abs(x) > self.max_x or abs(y) > self.max_y:
+            raise IndexError('(%u, %u) is not within (-+%u, -+%u)' % (x, y, self.max_x, self.max_y))
+        return collections.defaultdict.__missing__(self, key)
+
+
+class TimePeriod(object):
+    def __init__(self, factory):
+        self.area = factory()
+
+def rectangularAreas(x, y):
     def generator():
-        fact = lookupFactory(x, y)
-        far_past, past, present = (TimePeriod(fact) for x in xrange(3))
+        fact = lambda: RectangularArea(x, y)
+        far_past, past, present = (TimePeriod(fact) for n in xrange(3))
 
         """
         put map building logic here
@@ -31,12 +37,10 @@ def standard(x, y):
         return far_past, past, present
     return generator
 
-
-class TimePeriod(object):
-    def __init__(self, factory):
-        area = collections.defaultdict(factory)
-
-
 class GameWorld(object):
     def __init__(self, generator):
         self.far_past, self.past, self.present = generator()
+
+class RectangularGameWorld(GameWorld):
+    def __init__(self, x, y):
+        GameWorld.__init__(self, rectangularAreas(x, y))
--- server/game/test/game_test.py
+++ server/game/test/game_test.py
@@ -1,14 +1,38 @@
 import server.game.base as base
+import unittest
+import collections
 
 
-def test():
-    assert False
+class TestBaseObjects(unittest.TestCase):
 
-def passtest():
-    pass
+    def setUp(self):
+        self.world = base.RectangularGameWorld(10, 10)
 
-def test_build_world():
-    assert base.GameWorld(base.standard(10, 10)) != None
-    
+    def test_build_world(self):
+        self.assertFalse(self.world == None)
+        for k in (self.world.far_past, self.world.past, self.world.present):
+            self.assertFalse(k == None)
+            self.assertFalse(k.area == None)
 
+    def test_lookup(self):
+        for k in (self.world.far_past, self.world.past, self.world.present):
+            self.assertTrue(isinstance(k.area, collections.defaultdict))
+            try:
+                k.area[1]
+            except Exception as e:
+                self.assertTrue(isinstance(e, TypeError))
+            try:
+                k.area[(1,2,3)]
+            except Exception as e:
+                self.assertTrue(isinstance(e, TypeError))
+            try:
+                k.area[(11,10)]
+            except Exception as e:
+                self.assertTrue(isinstance(e, IndexError))
+            e = 10
+            try:
+                k.area[(0,0)].append(e)
+                self.assertTrue(e in k.area[(0,0)])
+            except Exception as e:
+                self.fail()
 
