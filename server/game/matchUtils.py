9aedbc097bcbe30028c50bdf4eeed83d4c5af91c
sgmr29 <sgmr29@hms-beatdown.(none)>
2009-08-20 12:22:12 -0500

    Fixed my decorator for checking id references.

--- server/game/matchUtils.py
+++ server/game/matchUtils.py
@@ -1,11 +1,11 @@
 from functools import wraps
 
-def verifyReferences(self, expression, *references):
+def verifyReferences(self, expression, references):
     for i in range(0, len(references)):
-        if referneces(i) is not None:
+        if references[i] is not None:
             if expression[i] not in self.objects:
                 return str(expression[i]) + " does not exist"
-            if not isinstance(self.objects(expression[i])):
+            if not isinstance(self.objects[expression[i]], references[i]):
                 return str(expression[i]) + " does not reference a " \
                     + references.__name__
 
@@ -13,8 +13,8 @@ def verifyReferences(self, expression, *references):
 def requireReferences(*n):
     def dec(f):
         @wraps(f)
-        def wrapper(self, expression):
-            if not verifyReferences(self, expression, *n):
+        def wrapper(self, *expression):
+            if not verifyReferences(self, expression, n):
                 return False
             return f(self, expression)
         return wrapper
