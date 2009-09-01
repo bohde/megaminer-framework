ad1f7d1e7140ae152e4f1c00257075549a748149
Stephen Mues <sgmr29@mst.edu>
2009-09-01 15:38:00 -0500

    Added status messages to be sent from the server.  Began work on sending only changed objects

--- server/game/buildingType.py
+++ server/game/buildingType.py
@@ -1,4 +1,24 @@
 from gameObject import *
 
 class BuildingType(GameObject):
-    pass
+    def __init__(self, game):
+        GameObject.__init__(self, game)
+        self.name = "Unknown"
+        self.price = 0
+        self.food = 0
+        self.buildTime = 0
+        self.hp = 0
+        self.armor = 0
+        self.builtBy = ""
+        self.allowPaint = 0
+
+    def toList(self):
+        list = GameObject.toList(self)
+        if (self.builtBy is not None):
+            builderID = self.builtBy.id
+        else:
+            builderID = -1
+        list.extend([self.name, self.price, self.food, self.buildTime,
+                     self.hp, self.armor, builderID, self.allowPaint])
+        return list
+
--- server/game/unitType.py
+++ server/game/unitType.py
@@ -1,5 +1,33 @@
 from gameObject import *
 
 class UnitType(GameObject):
-    pass
+    def __init__(self, game):
+        GameObject.__init__(self, game)
+        self.name = "Unknown"
+        self.price = 0
+        self.hunger = 0
+        self.trainTime = 0
+        self.hp = 0
+        self.armor = 0
+        self.moves = 0
+        self.actions = 0
+        self.attackCost = 0
+        self.damage = 0
+        self.minRange = 0
+        self.maxRange = 0
+        self.trainedBy = ""
+        self.canPaint = 0
+
+    def toList(self):
+        list = GameObject.toList(self)
+        if (self.trainedBy is not None):
+            trainerID = self.trainedBy.id
+        else:
+            trainerID = -1
+        list.extend([self.name, self.price, self.hunger, self.trainTime,
+                     self.hp, self.armor, self.moves, self.actions,
+                     self.attackCost, self.damage, self.minRange,
+                     self.maxRange, trainerID, self.canPaint])
+        return list
+
 
