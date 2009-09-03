5f77a5606b107308ddca0d43970e71ec043fe0fe
Stephen Mues <sgmr29@mst.edu>
2009-09-03 14:06:59 -0500

    Created in-game protocols

--- server/filters/GameFilter.py
+++ server/filters/GameFilter.py
@@ -1,4 +1,8 @@
 from LogicFilter import LogicFilter
+import statements.Statements as GameStatements
 
 class GameFilter(LogicFilter):
-    pass
+     def _init(self):
+        LogicFilter._init(self)
+        self.statements = GameStatements.statements
+
