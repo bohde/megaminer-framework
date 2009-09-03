b2faa91ff863c9e2b5d14a5efc727b43405a1d3b
Stephen Mues <sgmr29@mst.edu>
2009-09-03 11:14:54 -0500

    Cleaned up Statements.py

--- server/statements/Statements.py
+++ server/statements/Statements.py
@@ -21,7 +21,7 @@
 import sexpr
 import random
 import math
-#import Game
+from game.match import Match
 from StatementUtils import dict_wrapper, require_length, require_game
 #from db import Database
 #from db import Game as game_log
@@ -32,10 +32,11 @@ client_version = 2
 statements = {}
 wrapper = dict_wrapper(statements)
 
+"""
 @wrapper('request-log')
 @require_length(2)
 def requestLog(self, expression):
-    #try:
+    try:
         logID = str(expression[1])
         if game_log.DB is None:
             db = Database()        
@@ -43,10 +44,11 @@ def requestLog(self, expression):
             db = game_log.DB
         self.writeSExpr(['log', logID, db.lookup_game(logID)])
         return True
-    #except:
+    except:
         self.writeSExpr(['request-log-denied', 'Log unavailable'])
         return False
-    #return True
+    return True
+"""
 
 @wrapper('create-game')
 @require_length(1,2)
@@ -102,8 +104,8 @@ def joinGame(self, expression):
     try:
         type = expression[2]
     except:
-        type = None
-    if type not in ['zombie', 'human', 'spectator']:
+        type = 'player'
+    if type not in ['player', 'spectator']:
         self.writeSExpr(['join-denied', 'invalid player type'])
         return False
         
@@ -112,11 +114,11 @@ def joinGame(self, expression):
     if game not in games:
         self.writeSExpr(['join-denied', 'no such game'])
         return False
-    if not games[game].addPlayer(self):
-        #self.writeSExpr(['join-denied', 'game full'])
-        return False
     if self.game is not None:
         self.writeSExpr(['join-denied', 'already in a game'])
+        return False
+    if not games[game].addPlayer(self, type):
+        self.writeSExpr(['join-denied', 'game full'])
         return False
     
     self.writeSExpr(['join-accepted', type])
@@ -129,34 +131,6 @@ def notify(self):
     for i in self.Connections.values():
         if not i == self:
             i.writeSExpr(['notification', self.user, games[self.game].id])
-
-#todo: remove
-@wrapper('spectate-game')
-@require_length(2)
-def spectateGame(self, expression):
-    if not self.user:
-        self.writeSExpr(['spectate-denied', 'not logged in'])
-        return False
-    
-    try:
-        game = int(expression[1])
-    except:
-        game = None
-    
-    if game not in games:
-        self.writeSExpr(['spectate-denied', 'no such game'])
-        return False
-    
-    if not games[game].addSpectator(self):
-        self.writeSExpr(['spectate-denied', 'game full'])
-        return False
-    
-    if self.game is not None:
-        self.writeSExpr(['spectate-denied', 'already in a game'])
-        return False
-    self.writeSExpr(['spectate-accepted'])
-    self.game = game
-    return True
 
 @wrapper('leave-game')
 @require_length(1)
@@ -175,21 +149,23 @@ def myGame(self, expression):
     self.writeSExpr(['your-game', self.game])
     return True
 
+"""
 @wrapper('game-chat')
 @require_length(2)
 @require_game
 def gameChat(self, expression):
     games[self.game].chat(self, expression[1])
     return True
+"""
 
 @wrapper('game-start')
 @require_length(1)
 @require_game
 def gameStart(self, expression):
-    if not games[self.game].start():
-        self.writeSExpr(['game-denied', 'not ready'])
+    errBuff = games[self.game].start()
+    if errBuff != True:
+        self.writeSExpr(['game-denied', errBuff])
         return False
-    #self.writeSExpr(['game-start-accepted'])
     return True
 
 @wrapper('winner')
@@ -235,7 +211,7 @@ def gameMove(self, expression):
     return True
 
 @wrapper('game-attack')
-@require_length(3)
+@require_length(4)
 @require_game
 def gameAttack(self, expression):
     if games[self.game].turn != self:
@@ -243,123 +219,20 @@ def gameAttack(self, expression):
         return False
     try:
         id = int(expression[1])
-        targetID = int(expression[2])
+        x = int(expression[2])
+        y = int(expression[3])
     except:
         self.writeSExpr(['game-attack-denied', 'arguments not integers'])
         return False
 
-    errBuff = games[self.game].attack(id, targetID)
+    errBuff = games[self.game].attack(id, x, y)
     if errBuff != True:
         self.writeSExpr(['game-attack-denied', errBuff])
         return False
     return True
 
-@wrapper('game-attack-ground')
-@require_length(4)
-@require_game
-def gameAttackGround(self, expression):
-    if games[self.game].turn != self:
-        self.writeSExpr(['game-attack-ground-denied', 'not your turn'])
-        return False
-    try:
-        id = int(expression[1])
-        x = int(expression[2])
-        y = int(expression[3])
-    except:
-        self.writeSExpr(['game-attack-ground-denied', 'arguments not integers'])
-        return False
-
-    errBuff = games[self.game].attack(id, x, y)
-    if errBuff != True:
-        self.writeSExpr(['game-attack-groud-denied', errBuff])
-        return False
-    return True
-
-@wrapper('game-eat')
-@require_length(3)
-@require_game
-def gameEat(self, expression):
-    if games[self.game].turn != self:
-        self.writeSExpr(['game-eat-denied', 'not your turn'])
-        return False
-    try:
-        id = int(expression[1])
-        targetID = int(expression[2])
-    except:
-        self.writeSExpr(['game-eat-denied', 'arguments not integers'])
-        return False
-
-    errBuff = games[self.game].eat(id, targetID)
-    if errBuff != True:
-        self.writeSExpr(['game-eat-denied', errBuff])
-        return False
-    return True
-
-@wrapper('game-turn')
-@require_length(3)
-@require_game
-def gameTurn(self, expression):
-    if games[self.game].turn != self:
-        self.writeSExpr(['game-turn-denied', 'not your turn'])
-        return False
-    try:
-        id = int(expression[1])
-        direction = int(expression[2])
-    except:
-        self.writeSExpr(['game-turn-denied', 'arguments not integers'])
-        return False
-
-    errBuff = games[self.game].rotate(id, direction)
-    if errBuff != True:
-        self.writeSExpr(['game-turn-denied', errBuff])
-        return False
-    return True
-
-@wrapper('game-grab')
-@require_length(3)
-@require_game
-def gameGrab(self, expression):
-    if games[self.game].turn != self:
-        self.writeSExpr(['game-grab-denied', 'not your turn'])
-        return False
-    try:
-        id = int(expression[1])
-        targetID = int(expression[2])
-    except:
-        self.writeSExpr(['game-grab-denied', 'arguments not integers'])
-        return False
-
-    errBuff = games[self.game].grab(id, targetID)
-    if errBuff != True:
-        self.writeSExpr(['game-grab-denied', errBuff])
-        return False
-    return True
-
-@wrapper('game-throw')
-@require_length(6)
-@require_game
-def gameThrow(self, expression):
-    if games[self.game].turn != self:
-        self.writeSExpr(['game-throw-denied', 'not your turn'])
-        return False
-    try:
-        id = int(expression[1])
-        itemID = int(expression[2])
-        targetX = int(expression[3])
-        targetY = int(expression[4])
-        quantity = int(expression[5])
-    except:
-        self.writeSExpr(['game-throw-denied', 'arguments not integers'])
-        return False
-
-    errBuff = games[self.game].throw(id, itemID, targetX, targetY, quantity)
-    if errBuff != True:
-        self.writeSExpr(['game-throw-denied', errBuff])
-        return False
-    return True
-
 @wrapper('game-build')
-@require_length(4)
+@require_length(5)
 @require_game
 def gameBuild(self, expression):
     if games[self.game].turn != self:
@@ -369,118 +242,74 @@ def gameBuild(self, expression):
         id = int(expression[1])
         x = int(expression[2])
         y = int(expression[3])
+        buildingTypeID = int(expression[4])
     except:
         self.writeSExpr(['game-build-denied', 'arguments not integers'])
         return False
 
-    errBuff = games[self.game].buildWall(id, x, y)
+    errBuff = games[self.game].build(id, x, y, buildingTypeID)
     if errBuff != True:
         self.writeSExpr(['game-build-denied', errBuff])
         return False
     return True
 
-#def gameCombine(self, expression):
-#    if not verifyLength(self, expression, 3):
-#        return False
-#    if self.game == None:
-#        self.writeSExpr(['game-combine-denied', 'no game'])
-#        return False
-#    if games[self.game].turn != self:
-#        self.writeSExpr(['game-combine-denied', 'not your turn'])
-#        return False
-#    try:
-#        id = int(expression[1])
-#        target = int(expression[2])
-#    except:
-#        self.writeSExpr(['game-combine-denied', 'arguments not integers'])
-#        return False
-#
-#    errBuff = games[self.game].combineUnits(id, target)
-#    if errBuff != True:
-#        self.writeSExpr(['game-combine-denied', errBuff])
-#        return False
-#    return True
-
-@wrapper('game-human')
-@require_length(4)
-@require_game
-def gameHuman(self, expression):
-    if games[self.game].turn != self:
-        self.writeSExpr(['game-human-denied', 'not your turn'])
-        return False
-    try:
-        x = int(expression[1])
-        y = int(expression[2])
-        weaponID = int(expression[3])
-    except:
-        self.writeSExpr(['game-human-denied', 'arguments not integers'])
-        return False
-
-    errBuff = games[self.game].spawnHuman(x, y, weaponID)
-    if errBuff != True:
-        self.writeSExpr(['game-human-denied', errBuff])
-        return False
-    return True
-
-@wrapper('game-zombie')
+@wrapper('game-train')
 @require_length(3)
 @require_game
-def gameZombie(self, expression):
+def gameTrain(self, expression):
     if games[self.game].turn != self:
-        self.writeSExpr(['game-zombie-denied', 'not your turn'])
+        self.writeSExpr(['game-train-denied', 'not your turn'])
         return False
     try:
-        spawnZoneID = int(expression[1])
-        facing = int(expression[2])
+        id = int(expression[1])
+        unitTypeID = int(expression[2])
     except:
-        self.writeSExpr(['game-zombie-denied', 'arguments not integers'])
+        self.writeSExpr(['game-train-denied', 'arguments not integers'])
         return False
 
-    errBuff = games[self.game].spawnZombie(spawnZoneID, facing)
+    errBuff = games[self.game].train(id, unitTypeID)
     if errBuff != True:
-        self.writeSExpr(['game-zombie-denied', errBuff])
+        self.writeSExpr(['game-train-denied', errBuff])
         return False
     return True
 
-@wrapper('game-airstrike')
+@wrapper('game-paint')
 @require_length(4)
 @require_game
-def gameAirstrike(self, expression):
+def gameTrain(self, expression):
     if games[self.game].turn != self:
-        self.writeSExpr(['game-airstrike-denied', 'not your turn'])
+        self.writeSExpr(['game-paint-denied', 'not your turn'])
         return False
     try:
-        itemID = int(expression[1])
+        id = int(expression[1])
         x = int(expression[2])
         y = int(expression[3])
     except:
-        self.writeSExpr(['game-airstrike-denied', 'arguments not integers'])
+        self.writeSExpr(['game-paint-denied', 'arguments not integers'])
         return False
 
-    errBuff = games[self.game].callAirstrike(x, y, itemID)
+    errBuff = games[self.game].paint(id, x, y)
     if errBuff != True:
-        self.writeSExpr(['game-airstrike-denied', errBuff])
+        self.writeSExpr(['game-paint-denied', errBuff])
         return False
     return True
 
-@wrapper('game-give')
-@require_length(4)
+@wrapper('game-cancel')
+@require_length(2)
 @require_game
-def gameGive(self, expression):
+def gameTrain(self, expression):
     if games[self.game].turn != self:
-        self.writeSExpr(['game-give-denied', 'not your turn'])
+        self.writeSExpr(['game-cancel-denied', 'not your turn'])
         return False
     try:
-        giverID = int(expression[1])
-        takerID = int(expression[2])
-        quantity = int(expression[3])
+        id = int(expression[1])
     except:
-        self.writeSExpr(['game-give-denied', 'arguments not integers'])
+        self.writeSExpr(['game-cancel-denied', 'arguments not integers'])
         return False
 
-    errBuff = games[self.game].giveAmmo(giverID, takerID, quantity)
+    errBuff = games[self.game].cancel(id)
     if errBuff != True:
-        self.writeSExpr(['game-give-denied', errBuff])
+        self.writeSExpr(['game-cancel-denied', errBuff])
         return False
     return True
 
