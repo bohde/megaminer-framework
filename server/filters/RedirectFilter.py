5ce560b92633190892713702cd8b5a124b6af4ab
Josh Bohde <josh.bohde@gmail.com>
2009-09-21 13:30:26 -0500

    Licensed and minor cleanup.
    Conflicts:
    
    	server/main.py
    
    Conflicts:
    
    	server/filters/GameFilter.py
    	server/statements/Statements.py

--- server/filters/RedirectFilter.py
+++ server/filters/RedirectFilter.py
@@ -1,3 +1,21 @@
+"""
+    Redirect Server implementation.
+    Copyright (C) 2009  Josh Bohde <josh.bohde@gmail.com>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""                                             
+
 from __future__ import with_statement
 from LogicFilter import LogicFilter
 import statements.RedirectStatements as RedirectStatements
@@ -8,8 +26,6 @@ class RedirectFilter(LogicFilter):
     GameNumber = 0
     Games = dict()
     GameLock = threading.Lock()
-
-
 
     def _init(self):
         LogicFilter._init(self)
--- server/visualizer/visualizer.py
+++ server/visualizer/visualizer.py
@@ -1,3 +1,21 @@
+"""
+    Class files for a game server, interacting with the redirect server.
+    Copyright (C) 2009  Josh Bohde <josh.bohde@gmail.com>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""                                             
+
 from networking.Client import Client
 from filters.LogicFilter import SexprHandlerMixin
 from statements.Statements import require_length, dict_wrapper
