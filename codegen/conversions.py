8ca383ebac00d0e0e968d6bc4316fb396ca413ef
Steven Wallace <snw275@mst.edu>
2009-09-26 17:17:26 +0000

    Unit function stuff

--- codegen/conversions.py
+++ codegen/conversions.py
@@ -1,1 +1,11 @@
+from util import *
+
+import structures
+
+import data
+
 c = {int:'int', str:'char*'}
+
+for i in members(data):
+    if isinstance(i, structures.Model):
+      c[i] = c[i.data[i.key]]
