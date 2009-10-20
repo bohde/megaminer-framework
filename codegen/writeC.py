e7fdb09bb874b5a901e965d72c678cbae54609b8
Steven Wallace <snw275@mst.edu>
2009-10-20 20:14:22 +0000

    "Bound globals to the C++ client."

--- codegen/writeC.py
+++ codegen/writeC.py
@@ -2,9 +2,11 @@ from mako.template import Template
 import glob
 
 import conversions
+import util
 
 def write(data):
   data['conversions'] = conversions.c
+  data['capitalize'] = util.capitalize
   for file in glob.glob('templates/c/files/*.txt'):
     writeFile(file[18:-4], data) #file[18:-4] should strip the templates/c/files/ and .txt
 
