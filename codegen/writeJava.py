dd2abbf512af80189146bf6bb8398d2dc08f2735
cherez <snwallace@gmail.com>
2009-10-27 20:59:53 +0000

    Java client almost done!

--- codegen/writeJava.py
+++ codegen/writeJava.py
@@ -12,10 +12,19 @@ def write(data):
   data['Model'] = structures.Model
   for file in glob.glob('templates/java/files/*.txt'):
     writeFile(file[21:-4], data) #file[18:-4] should strip the templates/c/files/ and .txt
+  for model in data['models']:
+    data['model'] = model
+    writeModel(data)
 
 def writeFile(name, data):
   template = Template(filename='templates/java/files/%s.txt' % name)
   output = file('output/java/%s' % name, 'w')
+  output.write(template.render(**data))
+  output.close()
+
+def writeModel(data):
+  template = Template(filename='templates/java/model.txt')
+  output = file('output/java/%s.java' % data['model'].name, 'w')
   output.write(template.render(**data))
   output.close()
 
