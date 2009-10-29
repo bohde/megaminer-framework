87babd9dc43032aab04fda613e04c0902391cd8a
Josiah Worcester <josiahw@gmail.com>
2009-10-28 19:43:06 -0500

    And giving *all* my changes.

--- codegen/conversions.py
+++ codegen/conversions.py
@@ -6,8 +6,11 @@ import data
 
 c = {int:'int', str:'char*'}
 java = {int:'int', str:'String'}
+python = {int:'c_int', str:'c_char_p'}
 
 for i in members(data):
     if isinstance(i, structures.Model):
       c[i] = '_' + i.name + '*' 
       java[i] = 'Pointer'
+      python[i] = 'c_void_p'
+
