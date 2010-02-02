4a0b4d7e71b15fc412b3bce042593d7a02ca42cb
Josh Bohde <josh.bohde@gmail.com>
2010-02-01 20:06:38 -0600

    Added metaclass definition to autoregister protocol methods.

--- networking/apps.py
+++ networking/apps.py
@@ -1,35 +1,79 @@
 from abc import ABCMeta
+from collections import defaultdict
+from functools import wraps
+
+def namedmethod(name):
+    def inner(f):
+        f._name = name
+        print name
+        f.is_protocol = True
+        return f
+    return inner
+
+def protocolmethod(f):
+    name = f.__name__.lower().replace("_","-")
+    return namedmethod(name)(f)
+
+class Protocol(type):
+    def __new__(cls, name, bases, attrs):
+        _mapper = {}
+        for attrname, attrvalue in attrs.iteritems():
+            if getattr(attrvalue, 'is_protocol', 0):
+                _mapper[attrvalue._name] = attrvalue
+        attrs["_mapper"] = _mapper
+        return super(Protocol, cls).__new__(cls, name, bases, attrs)
 
 class BaseApp(object):
-    __metaclass__ = ABCMeta
-
+    __metaclass__ = Protocol
+    wrapper = {}
+    
     def __init__(self, protocol):
         self.protocol = protocol
-    
+
     def run(self, commands):
         def apply(coms):
-            return self.__class__.mapper[coms[0]](self, coms[1:])
+            try:
+                command = self.__class__._mapper[coms[0]] 
+                return command(self, coms[1:])
+            except KeyError as e:
+                return "invalid-command: %s" % coms[0]
+            except Exception as e:
+                return "unknown-error", ("command", coms[0]), ("arguments", coms[1:])
         return filter(bool, map(apply, commands))
 
 class LoggingApp(BaseApp):
     pass
 
-
 class AccountsApp(BaseApp):
     def __init__(self):
         self.logged_in = False
+        super(AccountsApp, self).__init__()
     
     def login(self, name, password):
         if name=="foo" and password=="bar":
             self.logged_in = True
-            return "Success!"
-        return "Failure"
+        return self.logged_in
 
     def logout(self):
         self.logged_in = False
 
+    def status(self):
+        return self.logged_in
+
 class CompetitorsApp(BaseApp):
-    pass
+    wins = defaultdict(int)
+    games = defaultdict(int)
+
+    def win(self, user):
+        wins[user] += 1
+
+    def game(self, user):
+        games[user] += 1
+
+    def user_info(self, user):
+        return {"user":user,
+                "wins":wins[user],
+                "games":games[user]}
 
 class GameApp(BaseApp):
     #@Protocol 
--- networking/test.py
+++ networking/test.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 from dispatch import DispatchProtocol
-from apps import BaseApp
+from apps import BaseApp, protocolmethod, namedmethod
 from itertools import repeat
 from twisted.internet import protocol, reactor
 
@@ -10,33 +10,31 @@ def fact(n):
     return reduce(lambda x,y: x*y, xrange(1,n))
 
 class Ping(BaseApp):
+    @protocolmethod
     def ping(self, *args):
         return "pong"
 
+    @protocolmethod
     def fat_ping(self, *args):
         return [["pong"] for x in xrange(10000)]
 
+    @protocolmethod
     def null(self, *args):
         return self.value
 
+    @namedmethod("burn")
     def burn(self, *args):
         self.value = fact(5)
         return None
 
+    @protocolmethod
     def fat_burn(self, *args):
         fact(1000)
         return [["phat"] for x in xrange(10)]
 
+    @protocolmethod
     def whoami(self, *args):
         return self.protocol.session_num
-
-    mapper = {"ping":ping,
-              "fat-ping":fat_ping,
-              "null": null,
-              "burn":burn,
-              "fat-burn":fat_burn,
-              "whoami":whoami,
-              }
 
 class TestLatencyServer(DispatchProtocol):
     apps = {
